4e52723c83b5241030a5cf589ddc13828758fd3f
walter.hwang <lyvius2@naver.com>
2024-01-13 02:19:41 +0900

    나눗셈 테스트 코드

--- src/test/kotlin/com/group/libraryapp/calculator/CalculatorTest.kt
+++ src/test/kotlin/com/group/libraryapp/calculator/CalculatorTest.kt
@@ -5,6 +5,8 @@ fun main() {
     calculatorTest.addTest()
     calculatorTest.minusTest()
     calculatorTest.multiplyTest()
+    calculatorTest.divideTest()
+    calculatorTest.divideExceptionTest()
 }
 
 internal class CalculatorTest {
@@ -46,6 +48,38 @@ internal class CalculatorTest {
         if (calculator.number != 15) {
             throw IllegalStateException()
         }
+    }
+
+    fun divideTest() {
+        // given
+        val calculator = Calculator(5)
+
+        // when
+        calculator.divide(2)
+
+        // then
+        if (calculator.number != 2) {
+            throw IllegalStateException()
+        }
+    }
+
+    fun divideExceptionTest() {
+        // given
+        val calculator = Calculator(5)
+
+        // when
+        try {
+            calculator.divide(0)
+        } catch (e: IllegalArgumentException) {
+            if (e.message != "0으로 나눌 수 없습니다.") {
+                throw IllegalStateException("메시지가 다릅니다.")
+            }
+            // Test Success
+            return
+        } catch (e: Exception) {
+            throw IllegalStateException()
+        }
+        throw IllegalStateException("기대하는 예외가 발생하지 않았습니다.")
     }
 
 }
