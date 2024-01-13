c6ea00956e0d8271ad46e334026f1faba3215456
lyvius2 <lyvius2@naver.com>
2024-01-13 18:35:05 +0900

    도메인 객체 kotlin 리팩토링 (User)

---
+++ src/main/java/com/group/libraryapp/dto/book/request/BookRequest.java
@@ -0,0 +1,14 @@
+package com.group.libraryapp.dto.book.request;
+
+public class BookRequest {
+
+  private String name;
+
+  public String getName() {
+    return name;
+  }
+
+  public BookRequest(String name) {
+    this.name = name;
+  }
+}
--- src/test/kotlin/com/group/libraryapp/service/user/UserServiceTest.kt
+++ src/test/kotlin/com/group/libraryapp/service/user/UserServiceTest.kt
@@ -2,8 +2,8 @@ package com.group.libraryapp.service.user
 
 import com.group.libraryapp.domain.user.User
 import com.group.libraryapp.domain.user.UserRepository
-import com.group.libraryapp.dto.user.UserCreateRequest
-import com.group.libraryapp.dto.user.UserUpdateRequest
+import com.group.libraryapp.dto.user.request.UserCreateRequest
+import com.group.libraryapp.dto.user.request.UserUpdateRequest
 import org.assertj.core.api.AssertionsForInterfaceTypes.assertThat
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.DisplayName
@@ -60,10 +60,10 @@ class UserServiceTest @Autowired constructor(
     fun updateUserTest() {
         // given
         val savedUser = userRepository.save(User("A", null))
-        val request = UserUpdateRequest(savedUser.id, "B")
+        val request = UserUpdateRequest(savedUser.id!!, "B")
 
         // when
-        userService.updateUser(request)
+        userService.updateUserName(request)
 
         // then
         val result = userRepository.findAll()[0]
