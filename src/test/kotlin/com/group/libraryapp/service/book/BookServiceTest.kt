70f8fa18dd59d48774e73d7ec142c6bdc14d9229
lyvius2 <lyvius2@naver.com>
2024-01-14 16:50:13 +0900

    책 통계 기능 테스트 코드 추가

--- src/test/kotlin/com/group/libraryapp/service/book/BookServiceTest.kt
+++ src/test/kotlin/com/group/libraryapp/service/book/BookServiceTest.kt
@@ -11,6 +11,7 @@ import com.group.libraryapp.domain.user.loanhistory.UserLoanStatus
 import com.group.libraryapp.dto.book.request.BookLoanRequest
 import com.group.libraryapp.dto.book.request.BookRequest
 import com.group.libraryapp.dto.book.request.BookReturnRequest
+import com.group.libraryapp.dto.book.response.BookStatResponse
 import org.assertj.core.api.AssertionsForInterfaceTypes.assertThat
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.DisplayName
@@ -103,5 +104,59 @@ class BookServiceTest @Autowired constructor(
         val results = userLoanHistoryRepository.findAll()
         assertThat(results).hasSize(1)
         assertThat(results[0].status).isEqualTo(UserLoanStatus.RETURNED)
+    }
+
+    @Test
+    @DisplayName("대출 중인 책의 권수를 반환한다.")
+    fun countLoanBookTest() {
+        // given
+        val savedUser = userRepository.save(User("A", null))
+        userLoanHistoryRepository.saveAll(listOf(
+            UserLoanHistory.fixture(savedUser, "케네디 평전"),
+            UserLoanHistory.fixture(savedUser, "클린코드", UserLoanStatus.RETURNED),
+            UserLoanHistory.fixture(savedUser, "코틀린 쿡북", UserLoanStatus.RETURNED)
+        ))
+
+        // when
+        val results = bookService.countLoanBook()
+
+        // then
+        assertThat(results).isEqualTo(1)
+    }
+
+    @Test
+    @DisplayName("분야별 책 권수를 반환한다.")
+    fun getBooksStatisticsTest() {
+        // given
+        bookRepository.saveAll(listOf(
+            Book.fixture("케네디 평전", BookType.SOCIETY),
+            Book.fixture("클린코드", BookType.COMPUTER),
+            Book.fixture("코틀린 쿡북", BookType.COMPUTER),
+        ))
+
+        // when
+        val results = bookService.getBookStatistics()
+
+        // then
+        assertThat(results).hasSize(2)
+        assertCount(results, BookType.COMPUTER, 2)
+        assertCount(results, BookType.SOCIETY, 1)
+        /*
+        val computerBook = results.first {
+            book -> book.type == BookType.COMPUTER
+        }
+        assertThat(computerBook.count).isEqualTo(2)
+        val societyBook = results.first {
+            book -> book.type == BookType.SOCIETY
+        }
+        assertThat(societyBook.count).isEqualTo(1)
+         */
+    }
+
+    private fun assertCount(results: List<BookStatResponse>, type: BookType, count: Int) {
+        val bookTypes = results.first {
+            book -> book.type == type
+        }
+        assertThat(bookTypes.count).isEqualTo(count)
     }
 }
