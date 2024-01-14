d268b4d88a1072d86ca1d3030eac7a0d13e5e934
lyvius2 <lyvius2@naver.com>
2024-01-14 15:08:23 +0900

    사용자 책 대출 현황 조회 서비스 테스트 코드 추가

--- src/test/kotlin/com/group/libraryapp/service/user/UserServiceTest.kt
+++ src/test/kotlin/com/group/libraryapp/service/user/UserServiceTest.kt
@@ -2,6 +2,9 @@ package com.group.libraryapp.service.user
 
 import com.group.libraryapp.domain.user.User
 import com.group.libraryapp.domain.user.UserRepository
+import com.group.libraryapp.domain.user.loanhistory.UserLoanHistory
+import com.group.libraryapp.domain.user.loanhistory.UserLoanHistoryRepository
+import com.group.libraryapp.domain.user.loanhistory.UserLoanStatus
 import com.group.libraryapp.dto.user.request.UserCreateRequest
 import com.group.libraryapp.dto.user.request.UserUpdateRequest
 import org.assertj.core.api.AssertionsForInterfaceTypes.assertThat
@@ -15,6 +18,7 @@ import org.springframework.test.context.ActiveProfiles
 @SpringBootTest
 @ActiveProfiles("local")
 class UserServiceTest @Autowired constructor(
+    private val userLoanHistoryRepository: UserLoanHistoryRepository,
     private val userRepository: UserRepository,
     private val userService: UserService,
 ) {
@@ -81,6 +85,81 @@ class UserServiceTest @Autowired constructor(
 
         // then
         assertThat(userRepository.findAll()).isEmpty()
+    }
+
+    @Test
+    @DisplayName("대출 기록이 없는 사용자도 응답에 포함된다.")
+    fun getUserLoanHistoriesTest1() {
+        // given
+        userRepository.save(User("A", null))
+
+        // when
+        val results = userService.getUserLoanHistories()
+
+        // then
+        assertThat(results).hasSize(1)
+        assertThat(results[0].name).isEqualTo("A")
+        assertThat(results[0].books).isEmpty()
+    }
+
+    @Test
+    @DisplayName("여러 대출 기록이 있는 사용자가 응답이 포함된다.")
+    fun getUserLoanHistoriesTest2() {
+        // given
+        val savedUser = userRepository.save(User("A", null))
+        userLoanHistoryRepository.saveAll(listOf(
+            UserLoanHistory.fixture(savedUser, "케네디 평전", UserLoanStatus.LOANED),
+            UserLoanHistory.fixture(savedUser, "클린코드", UserLoanStatus.LOANED),
+            UserLoanHistory.fixture(savedUser, "코틀린 쿡북", UserLoanStatus.RETURNED)
+        )
+        )
+
+        // when
+        val results = userService.getUserLoanHistories()
+
+        // then
+        assertThat(results).hasSize(1)
+        assertThat(results[0].name).isEqualTo("A")
+        assertThat(results[0].books).hasSize(3)
+        assertThat(results[0].books).extracting("name")
+            .containsExactlyInAnyOrder("케네디 평전", "클린코드", "코틀린 쿡북")
+        assertThat(results[0].books).extracting("isReturn")
+            .containsExactlyInAnyOrder(false, false, true)
+    }
+
+    @Test
+    @DisplayName("여러 대출 기록이 있는 사용자, 대출 기록이 없는 사용자 모두가 응답이 포함된다.")
+    fun getUserLoanHistoriesTest3() {
+        // given
+        val savedUsers = userRepository.saveAll(listOf(
+            User("A", null),
+            User("B", null)
+        ))
+        userLoanHistoryRepository.saveAll(listOf(
+            UserLoanHistory.fixture(savedUsers[0], "케네디 평전", UserLoanStatus.LOANED),
+            UserLoanHistory.fixture(savedUsers[0], "클린코드", UserLoanStatus.LOANED),
+            UserLoanHistory.fixture(savedUsers[0], "코틀린 쿡북", UserLoanStatus.RETURNED)
+        ))
+
+        // when
+        val results = userService.getUserLoanHistories()
+
+        // then
+        assertThat(results).hasSize(2)
+        val userAResult = results.first {
+            user -> user.name == "A"
+        }
+        assertThat(userAResult.name).isEqualTo("A")
+        assertThat(userAResult.books).hasSize(3)
+        assertThat(userAResult.books).extracting("name")
+            .containsExactlyInAnyOrder("케네디 평전", "클린코드", "코틀린 쿡북")
+        assertThat(userAResult.books).extracting("isReturn")
+            .containsExactlyInAnyOrder(false, false, true)
+        val userBResult = results.first {
+            user -> user.name == "B"
+        }
+        assertThat(userBResult.name).isEqualTo("B")
+        assertThat(userBResult.books).isEmpty()
     }
 
     @AfterEach
