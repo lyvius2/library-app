42f9cd1774247243a177d3b6d16b351303a418bf
lyvius2 <lyvius2@naver.com>
2024-01-14 15:26:32 +0900

    미사용 import 제거

--- src/main/kotlin/com/group/libraryapp/domain/user/UserRepository.kt
+++ src/main/kotlin/com/group/libraryapp/domain/user/UserRepository.kt
@@ -1,7 +1,6 @@
 package com.group.libraryapp.domain.user
 
 import org.springframework.data.jpa.repository.JpaRepository
-import java.util.*
 
 interface UserRepository : JpaRepository<User, Long> {
     fun findByName(name: String): User?
