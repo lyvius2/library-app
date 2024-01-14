4cbb4edd81b05b5aa33b28189f66ee4faa70f9ec
walter.hwang <lyvius2@naver.com>
2024-01-14 22:23:40 +0900

    userLoanHistoryRepository 를 querydsl로 전환

--- src/main/kotlin/com/group/libraryapp/service/book/BookService.kt
+++ src/main/kotlin/com/group/libraryapp/service/book/BookService.kt
@@ -10,6 +10,7 @@ import com.group.libraryapp.dto.book.request.BookRequest
 import com.group.libraryapp.dto.book.request.BookReturnRequest
 import com.group.libraryapp.dto.book.response.BookStatResponse
 import com.group.libraryapp.repository.book.BookQueryDslRepository
+import com.group.libraryapp.repository.user.loanhistory.UserLoanHistoryQueryDslRepository
 import com.group.libraryapp.util.fail
 import org.springframework.stereotype.Service
 import org.springframework.transaction.annotation.Transactional
@@ -20,6 +21,7 @@ class BookService constructor(
     private val bookQueryDslRepository: BookQueryDslRepository,
     private val userRepository: UserRepository,
     private val userLoanHistoryRepository: UserLoanHistoryRepository,
+    private val userLoanHistoryQueryDslRepository: UserLoanHistoryQueryDslRepository,
 ) {
     @Transactional
     fun saveBook(request: BookRequest) {
@@ -30,7 +32,7 @@ class BookService constructor(
     @Transactional
     fun loanBook(request: BookLoanRequest) {
         val book = bookRepository.findByName(request.bookName) ?: fail()
-        if (userLoanHistoryRepository.findByBookNameAndStatus(request.bookName, UserLoanStatus.LOANED) != null) {
+        if (userLoanHistoryQueryDslRepository.find(request.bookName, UserLoanStatus.LOANED) != null) {
             throw IllegalArgumentException("진작 대출되어 있는 책입니다")
         }
         val user = userRepository.findByName(request.userName) ?: fail()
@@ -45,7 +47,7 @@ class BookService constructor(
 
     @Transactional(readOnly = true)
     fun countLoanBook(): Int {
-        return userLoanHistoryRepository.countByStatus(UserLoanStatus.LOANED).toInt()
+        return userLoanHistoryQueryDslRepository.count(UserLoanStatus.LOANED).toInt()
         //return userLoanHistoryRepository.findAllByStatus(UserLoanStatus.LOANED).size
     }
 
